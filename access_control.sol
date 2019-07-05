f2f5f95476832771d292581dfc8504ed9f2a3966
Payam-Almasian <payamalmasian953@gmail.com>
2019-07-06 02:32:40 +0430

    Update access_control.sol

--- access_control.sol
+++ access_control.sol
@@ -3,11 +3,20 @@ pragma solidity ^0.5.1;
 contract Access_Control {
     
     
+    struct  File
+    {
+       
+       string Name ;
+       string Access_Policy ;
+       mapping ( string => string)  CP_ABE ; 
+        
+        
+    }
+    
+    mapping ( uint256 => File)  Shared_Files ;
+    
     address public owner ; 
     
-    mapping ( uint256 => string)  Access_Policy ;
-    mapping ( uint256 => string)  File_Directory ;
-    mapping ( uint256 => string)  CP_ABE ;
 
     constructor() public {
     
@@ -15,41 +24,46 @@ contract Access_Control {
         
     }
     
+    uint numFiles;
     
-    function get_file_CP_ABE( uint256 FileID ) public view returns (string memory) {
-        return CP_ABE[FileID] ;
-    }
-    
-    
-    function set_file_CP_ABE( uint256 id , string memory ABE_KEY ) public {
+    function CREATE_FILE(  string memory Owner_File_Name   , string memory Owner_Access_Policy  , string memory Owner_ABE_KEY ,  string memory USER_ID  ) public {
+        
         require(msg.sender == owner);
-        CP_ABE[id] = ABE_KEY ;
+        
+        numFiles++;
+        
+        Shared_Files[numFiles] = File ( {Name:Owner_File_Name , Access_Policy:Owner_Access_Policy  });
+        Shared_Files[numFiles].CP_ABE[USER_ID] = Owner_ABE_KEY ;
+        
+        // File Uploaded_file = fooStruct({foo:1, fighter:2});
+        
+        
     }
     
     
-
-
-    function get_file_access_policy( uint256 FileID ) public view returns (string memory) {
-        return File_Directory[FileID] ;
+    function get_file_name( uint256 File_Name ) public view returns (string memory) {
+        
+        return Shared_Files[File_Name].Name ;
+        
+        
+    }
+    
+    function get_file_access_policy( uint256 File_Name ) public view returns (string memory) {
+        
+        return Shared_Files[File_Name].Access_Policy ;
+        
+        
+    }
+    
+    function get_file_access_policy( uint256 File_Name , string memory UserID ) public view returns (string memory) {
+        
+        return Shared_Files[File_Name].CP_ABE[UserID] ;
+        
+        
     }
     
     
-    function set_file_access_policy( uint256 id , string memory access_policy) public {
-        require(msg.sender == owner);
-        File_Directory[id] = access_policy ;
-    }
     
-    
-
-    function get_file_desc( uint256 FileID ) public view returns (string memory) {
-        return File_Directory[FileID] ;
-    }
-    
-    
-    function set_file_desc( uint256 id , string memory desc) public {
-        require(msg.sender == owner);
-        File_Directory[id] = desc ;
-    }
     
     
 }
